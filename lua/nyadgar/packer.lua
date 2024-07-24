4aab283a192efbef911c8ae9a36cc0514e84daad
Noam Yadgar <nyadgar@zscaler.com>
2024-07-24 11:22:43 +0300

    trouble

--- lua/nyadgar/packer.lua
+++ lua/nyadgar/packer.lua
@@ -77,4 +77,16 @@ return require("packer").startup(function(use)
 
 	use("leoluz/nvim-dap-go")
 	use("mfussenegger/nvim-dap")
+
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -201,6 +201,12 @@ _G.packer_plugins = {
     path = "/Users/nyadgar/.local/share/nvim/site/pack/packer/start/todo-comments.nvim",
     url = "https://github.com/folke/todo-comments.nvim"
   },
+  ["trouble.nvim"] = {
+    config = { "\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0" },
+    loaded = true,
+    path = "/Users/nyadgar/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    url = "https://github.com/folke/trouble.nvim"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/Users/nyadgar/.local/share/nvim/site/pack/packer/start/vim-fugitive",
@@ -209,14 +215,18 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: srcery
-time([[Config for srcery]], true)
-try_loadstring("\27LJ\2\n:\0\0\3\0\4\0\0066\0\0\0009\0\1\0009\0\2\0'\2\3\0B\0\2\1K\0\1\0\vsrcery\16colorscheme\bcmd\bvim\0", "config", "srcery")
-time([[Config for srcery]], false)
+-- Config for: trouble.nvim
+time([[Config for trouble.nvim]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
+time([[Config for trouble.nvim]], false)
 -- Config for: conform.nvim
 time([[Config for conform.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fconform\frequire\0", "config", "conform.nvim")
 time([[Config for conform.nvim]], false)
+-- Config for: srcery
+time([[Config for srcery]], true)
+try_loadstring("\27LJ\2\n:\0\0\3\0\4\0\0066\0\0\0009\0\1\0009\0\2\0'\2\3\0B\0\2\1K\0\1\0\vsrcery\16colorscheme\bcmd\bvim\0", "config", "srcery")
+time([[Config for srcery]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
