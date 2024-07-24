2e31fec83eb08c1e991770fb07d1d29d48e9883a
Noam Yadgar <nyadgar@zscaler.com>
2024-07-24 14:26:09 +0300

    diff colors

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ require("nyadgar.packer")
 require("nyadgar.set")
 
 vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
-
 vim.keymap.set("n", "<C-]>", vim.cmd.bnext)
 vim.keymap.set("n", "<C-[>", vim.cmd.bprevious)
+
+vim.cmd("autocmd ColorScheme * hi DiffAdd      guifg=NONE          guibg=#003300")
+vim.cmd("autocmd ColorScheme * hi DiffChange   guifg=NONE          guibg=#003355")
+vim.cmd("autocmd ColorScheme * hi DiffText     guifg=NONE          guibg=#887766")
