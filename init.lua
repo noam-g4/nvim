f9d90fce5e9a78073997925a0d9989cd6a36510b
noam-g4 <noam.g4@gmail.com>
2024-09-02 10:42:17 +0300

    migrated to lazy.nvim plugin manager

--- init.lua
+++ init.lua
@@ -1,12 +1,2 @@
-require("nyadgar.packer")
-require("nyadgar.set")
-
-vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
-vim.keymap.set("n", "<C-l>", vim.cmd.bnext)
-vim.keymap.set("n", "<C-h>", vim.cmd.bprevious)
-vim.keymap.set("n", "<Esc>", function() end)
-vim.keymap.set("i", "jj", "<ESC>", { silent = true })
-
-vim.cmd("autocmd ColorScheme * hi DiffAdd      guifg=NONE          guibg=#003300")
-vim.cmd("autocmd ColorScheme * hi DiffChange   guifg=NONE          guibg=#003355")
-vim.cmd("autocmd ColorScheme * hi DiffText     guifg=NONE          guibg=#887766")
+require("config.custom")
+require("config.lazy")
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+	{ "williamboman/mason.nvim", opts = {} },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+				},
+			})
+		end,
+	},
+}
