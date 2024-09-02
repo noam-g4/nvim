0bdee1f17e627eb8ba90f203cacef47a60a328f6
Noam Yadgar <nyadgar@zscaler.com>
2024-09-02 13:11:55 +0300

    setup treesitter

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,6 +1,7 @@
 return {
 	"ray-x/go.nvim",
 	config = function()
+		require("go").setup({})
 		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			pattern = "*.go",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,26 @@
+local function setup()
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = { "lua", "go", "json", "yaml", "markdown", "markdown_inline" },
+		sync_install = false,
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn", -- set to `false` to disable one of the mappings
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm",
+			},
+		},
+	})
+end
+
 return {
 	"nvim-treesitter/nvim-treesitter",
+	config = setup,
 }
