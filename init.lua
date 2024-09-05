2a63011f64c489ea5a5ba29a4601e6d9fbcefdcd
Noam Yadgar <nyadgar@zscaler.com>
2024-09-05 08:32:08 +0300

    improvements

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 require("config.custom")
 require("config.lazy")
+
+vim.opt.termguicolors = true
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,4 +1,17 @@
+local function setup()
+	require("go").setup({})
+
+	local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.go",
+		callback = function()
+			require("go.format").goimports()
+		end,
+		group = format_sync_grp,
+	})
+end
+
 return {
 	"ray-x/go.nvim",
-	opts = {},
+	config = setup,
 }
