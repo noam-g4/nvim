8b5e8fa1d5461a0b0a39a50724d0109ca655c059
Noam Yadgar <nyadgar@zscaler.com>
2025-09-18 22:22:07 +0300

    super tab for autocompletion; put function in local (git.lua); lua_ls with global 'vim' object

--- lua/config/git.lua
+++ lua/config/git.lua
@@ -1,34 +1,34 @@
 local module = {}
 
-function get_commit_sha_from_blame()
-  local line = vim.fn.line(".") -- The current line in the buffer
-  local blame_output = vim.fn.system({
-    "git",
-    "blame",
-    "-L",
-    line .. "," .. line, -- Limit git blame to the specific line
-    "--porcelain",       -- Porcelain mode gives detailed blame info
-    vim.fn.expand("%")   -- File path of the current buffer
-  })
+local function get_commit_sha_from_blame()
+	local line = vim.fn.line(".") -- The current line in the buffer
+	local blame_output = vim.fn.system({
+		"git",
+		"blame",
+		"-L",
+		line .. "," .. line, -- Limit git blame to the specific line
+		"--porcelain",     -- Porcelain mode gives detailed blame info
+		vim.fn.expand("%") -- File path of the current buffer
+	})
 
-  local commit_sha = string.match(blame_output, "^([0-9a-f]+)") -- Match the first field (commit hash)
-  
-  if commit_sha then
-    return commit_sha
-  else
-    print("Unable to retrieve commit SHA.")
-    return nil
-  end
+	local commit_sha = string.match(blame_output, "^([0-9a-f]+)") -- Match the first field (commit hash)
+
+	if commit_sha then
+		return commit_sha
+	else
+		print("Unable to retrieve commit SHA.")
+		return nil
+	end
 end
 
 function module.open_diffview_from_blame()
-  local commit_hash = get_commit_sha_from_blame()
+	local commit_hash = get_commit_sha_from_blame()
 
-  local file_path = vim.fn.expand("%")
-  if not file_path or file_path == "" then
-    vim.notify("No file associated with the current buffer.", vim.log.levels.ERROR)
-    return
-  end
+	local file_path = vim.fn.expand("%")
+	if not file_path or file_path == "" then
+		vim.notify("No file associated with the current buffer.", vim.log.levels.ERROR)
+		return
+	end
 
 	vim.cmd("DiffviewOpen " .. commit_hash .. " HEAD --view=closed -- " .. file_path)
 end
--- lua/config/lsp_configs/lua_ls.lua
+++ lua/config/lsp_configs/lua_ls.lua
@@ -3,6 +3,13 @@ local module = {}
 function module.get_config(capabilities)
 	local c = vim.lsp.config['lua_ls']
 	c['capabilities'] = capabilities
+	c['settings'] = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' },
+			}
+		}
+	}
 	return c
 end
 
